c95a9b97220b8d0547a704adbc5bf306b82c16e4
Patrick Hennessey <7787945+pahenn@users.noreply.github.com>
2024-02-10 20:12:32 -0600

    prototype directus

---
+++ pages/our-story/directus.vue
@@ -0,0 +1,12 @@
+<template>
+  <h1>{{ global.title }}</h1>
+  <p>{{ global.description }}</p>
+</template>
+
+<script setup>
+  const { $directus, $readItems } = useNuxtApp()
+
+  const { data: global } = await useAsyncData("global", () => {
+    return $directus.request($readItems("global"))
+  })
+</script>
