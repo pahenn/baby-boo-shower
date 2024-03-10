f84afcf54bdc00bd1ca10121bafbc54de705e79c
Patrick Hennessey <7787945+pahenn@users.noreply.github.com>
2024-03-10 18:53:55 -0500

    change registry copy

--- pages/registry.vue
+++ pages/registry.vue
@@ -22,16 +22,13 @@
 
 <template>
   <div v-if="page">
-    <UPageHero v-bind="page.hero">
-      <!-- <template #links>
-        <UPricingToggle
-          v-model="isYearly"
-          class="w-48"
-        />
-      </template> -->
-    </UPageHero>
-
     <UContainer>
+      <UPageHero
+        v-for="hero in page.hero"
+        v-bind="hero"
+      >
+      </UPageHero>
+
       <UPricingGrid>
         <UPricingCard
           v-for="(plan, index) in page.plans"
@@ -40,28 +37,5 @@
         />
       </UPricingGrid>
     </UContainer>
-    <!-- 
-    <ULandingSection>
-      <ULandingLogos>
-        <UIcon
-          v-for="icon in page.logos.icons"
-          :key="icon"
-          :name="icon"
-          class="w-12 h-12 flex-shrink-0 text-gray-500 dark:text-gray-400"
-        />
-      </ULandingLogos>
-    </ULandingSection>
-
-    <ULandingSection
-      :title="page.faq.title"
-      :description="page.faq.description"
-    >
-      <ULandingFAQ
-        :items="page.faq.items"
-        multiple
-        default-open
-        class="max-w-4xl mx-auto"
-      />
-    </ULandingSection> -->
   </div>
 </template>
